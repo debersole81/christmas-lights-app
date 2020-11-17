2b1842d6c0667d61dda4c7bbad912628ec23ee4f
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-17 13:49:58 -0500

    Created class component

---
+++ src/components/Container.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Container extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>This is a Christmas Lights App</h1>
+            </div>
+        )
+    }
+}
+
+export default Container
