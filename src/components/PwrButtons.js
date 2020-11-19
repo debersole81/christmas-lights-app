6db77c5502627ca352f137752e0a21ef59078add
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-19 10:40:17 -0500

    created on/off buttons

--- src/components/PwrButtons.js
+++ src/components/PwrButtons.js
@@ -3,7 +3,10 @@ import React from "react";
 class PwrButtons extends React.Component {
     render() {
         return(
-            <h1>The Power Buttons will live here.</h1>
+            <div>
+                <button>On</button>
+                <button>Off</button>
+            </div>
         );
     };
        
