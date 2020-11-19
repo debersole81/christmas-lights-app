1ffabe871a59d6c4b6036c8f515783aa42d154d8
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-19 17:46:47 -0500

    updated file

--- src/App.js
+++ src/App.js
@@ -6,12 +6,39 @@ import UserControls from "./components/UserControls";
 
 
 class App extends React.Component {
+constructor() {
+    super();
+        this.state = {}
+
+    handlePwrOnChange = (e) => {
+        let circle = document.getElementById("circle");
+        if (e.target.id === "play") {
+            circle.style.animationPlayState("running");
+        }
+    };
+
+    handlePwrOffChange = (e) => {
+        let circle = document.getElementById("circle");
+        if (e.target.id === "stop") {
+            circle.style.animation("none");
+            circle.style.background("#563260");
+        };
+    };
+};
+
+
     render() {
+        
+        const pwrControls = {
+            handlePwrOnChange: this.handlePwrOnChange,
+            handlePwrOffChange: this.handlePwrOffChange
+        };
+
         return(
             <div className="utilities">
                 <Lights />
                 <Title />
-                <UserControls />    
+                <UserControls {...pwrControls} />
             </div>
         );
     };
