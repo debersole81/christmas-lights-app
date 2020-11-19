6081ab60549c766aeca5d94ac414152b3b79e7d7
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-19 17:46:12 -0500

    updated JSX  expressions

--- src/components/UserControls.js
+++ src/components/UserControls.js
@@ -2,17 +2,13 @@ import React from "react";
 import PwrButtons from "./PwrButtons";
 import Speed from "./Speed";
 
-const circle = document.getElementsByClassName("circle");
-
-
-
 
 class UserControls extends React.Component {
     render() {
         return (
             <div>
-                <PwrButtons circle={circle} />
-                <Speed circle={circle} />
+                <PwrButtons />
+                <Speed />
             </div>
         );
     };
