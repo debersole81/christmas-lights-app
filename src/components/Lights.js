ebd7e388e338768ea266560f396c09627de30131
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-18 20:56:31 -0500

    updated JSX properties in the render method

---
+++ src/components/Lights.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+      
+
+class Lights extends React.Component {
+    render() {
+        return(
+            <div className = "main">
+                <div className = "circle red"></div>
+                <div className = "circle yellow"></div>
+                <div className = "circle blue"></div>
+                <div className = "circle green"></div>
+                <div className = "circle red"></div>
+                <div className = "circle yellow"></div>
+                <div className = "circle blue"></div>
+                <div className = "circle green"></div>
+            </div>
+        );
+    };
+};
+
+export default Lights
