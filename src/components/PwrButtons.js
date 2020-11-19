c6de190b8c838cd1f30bd0d1e495b3c8d64a23c7
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-18 20:57:34 -0500

    created header property in render method

---
+++ src/components/PwrButtons.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class PwrButtons extends React.Component {
+    render() {
+        return(
+            <h1>The Power Buttons will live here.</h1>
+        );
+    };
+       
+};
+
+export default PwrButtons
