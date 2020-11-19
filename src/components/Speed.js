0437f590a6dca22b549b034cd2de4de5b4954c6a
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-18 20:58:19 -0500

    created header property in render method

---
+++ src/components/Speed.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Speed extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>The Speed Controls will live here</h1>
+            </div>
+        );
+    };
+};
+
+export default Speed
